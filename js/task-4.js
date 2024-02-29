e0fe4213eac8806e9f85b40e87b068df70f22db4
Vadym033 <150912531+Vadym033@users.noreply.github.com>
2024-02-29 23:49:09 +0100

    home-work two modal

---
+++ js/task-4.js
@@ -0,0 +1,30 @@
+'use strict';
+function getShippingCost(country) {
+  const chinaDeliveryFee = 100;
+  const chileDeliveryFee = 250;
+  const australiaDeliveryFee = 170;
+  const jamaicaDeliveryFee = 120;
+  switch (country) {
+    case `China`:
+      return `Shipping to ${country} will cost ${chinaDeliveryFee} credits`;
+      break;
+    case `Chile`:
+      return `Shipping to ${country} will cost ${chileDeliveryFee} credits`;
+      break;
+    case `Australia`:
+      return `Shipping to ${country} will cost ${australiaDeliveryFee} credits`;
+      break;
+    case `Jamaica`:
+      return `Shipping to ${country} will cost ${jamaicaDeliveryFee} credits`;
+      break;
+    default:
+      return `Sorry, there is no delivery to your country`;
+  }
+}
+
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
